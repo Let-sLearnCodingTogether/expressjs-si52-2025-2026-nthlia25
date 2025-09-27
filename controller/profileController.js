08d9a5a5e448a27d6da8691fdaca131d1f9c005a
nthlia25 <nathaliamargaretha2506@gmail.com>
2025-09-27 18:42:13 +0700

    Create profileController.js

---
+++ controller/profileController.js
@@ -0,0 +1,12 @@
+export const publicProfile = () => {
+    (req, res) => {
+const username = req.params.username
+
+    res.render('public-profile', {
+        title : username,
+        username : username,
+        bio: "test 1234"
+    })
+}
+
+}
